b880dc368e3121c3d4b0b6e992249ff705438490
Andrea Negri <andrea.negri@gmail.com>
2017-11-15 13:43:58 +0100

    added gdb

--- provision/compilers.sh
+++ provision/compilers.sh
@@ -3,7 +3,8 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install --fix-missing \
 gcc-4.9 \
 g++-4.9 \
-gfortran-4.9
+gfortran-4.9 \
+gdb
 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 10
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 10
