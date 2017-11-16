6b2f6dbafe109304aa16c294b01516d8824435a7
Andrea Negri <andrea.negri@gmail.com>
2017-11-16 15:36:33 +0100

    added git-svn module

--- provision/git.sh
+++ provision/git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-add-repository -y ppa:git-core/ppa
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git git-svn
 git config --global alias.serve 'daemon --verbose --export-all --base-path=.git --reuseaddr --strict-paths .git/'
 git config --global alias.st status
 git config --global color.ui auto
