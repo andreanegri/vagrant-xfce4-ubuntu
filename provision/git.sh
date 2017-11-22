c492ae706d7c8152f2c549d84517ac9bc0f80f1c
Andrea Negri <andrea.negri@gmail.com>
2017-11-22 09:38:01 +0100

    added gitinspector tool

--- provision/git.sh
+++ provision/git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-add-repository -y ppa:git-core/ppa
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git git-svn
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git git-svn gitinspector
 git config --global alias.serve 'daemon --verbose --export-all --base-path=.git --reuseaddr --strict-paths .git/'
 git config --global alias.st status
 git config --global color.ui auto
