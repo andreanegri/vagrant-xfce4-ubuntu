e43adf1562b7b8b96cf3a24632dd84b62f9bc576
Andrea Negri <andrea.negri@gmail.com>
2018-03-28 21:08:12 +0200

    removed specific compilers, using specific version offered by distro

--- provision/compilers.sh
+++ provision/compilers.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install --fix-missing \
-gcc-4.9 \
-g++-4.9 \
-gfortran-4.9 \
-gfortran-6 \
+gcc \
+g++ \
+gfortran \
 gdb
-
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 10
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 10
-sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-4.9 10
-sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-4.9 10
